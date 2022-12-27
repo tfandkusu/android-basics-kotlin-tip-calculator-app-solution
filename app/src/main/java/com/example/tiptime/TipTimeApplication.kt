17ffa683408ef213502f457d1e4aceccdd9650a7
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-12-28 00:28:41 +0900

    8. Applying a custom theme overlay

--- app/src/main/java/com/example/tiptime/TipTimeApplication.kt
+++ app/src/main/java/com/example/tiptime/TipTimeApplication.kt
@@ -2,10 +2,14 @@ package com.example.tiptime
 
 import android.app.Application
 import com.google.android.material.color.DynamicColors
+import com.google.android.material.color.DynamicColorsOptions
 
 class TipTimeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        DynamicColors.applyToActivitiesIfAvailable(this)
+        DynamicColors.applyToActivitiesIfAvailable(
+            this,
+            DynamicColorsOptions.Builder().setThemeOverlay(R.style.AppTheme_Overlay).build()
+        )
     }
 }
